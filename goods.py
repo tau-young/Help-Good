a4ab510f8e2843087081f80dd4b81d7e7eba182f
tauyoung <tauyoung@foxmail.com>
2022-09-21 16:45:09 +0800

    Add version tag and behavior fixes

--- goods.py
+++ goods.py
@@ -1,7 +1,9 @@
 import cmd, sqlite3
 
+CLI_VERSION = '0.1.0'
+
 class goodsShell(cmd.Cmd):
-	intro = '欢迎使用 Goods 交互式环境。键入 help 或者 ? 查看所有命令。\n'
+	intro = 'Help Goods %s\n键入\'help\'或者\'?\'查看所有命令。' % CLI_VERSION
 	prompt = '(goods) '
 
 	def __init__(self):
@@ -10,26 +12,29 @@ class goodsShell(cmd.Cmd):
 		self.cur = self.conn.cursor()
 		self.cur.execute('CREATE TABLE IF NOT EXISTS GOODS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT NOT NULL);')
 
+	def print_records(self):
+		for record in self.cur.fetchall(): print(record[1])
+
 	def do_add(self, arg):
 		'添加一个物品。'
 		self.cur.execute('INSERT INTO GOODS (NAME) VALUES ("%s")' % arg)
 		self.conn.commit()
 	def do_del(self, arg):
 		'删除一个物品。'
-		self.cur.execute('DELETE FROM GOODS WHERE NAME = "%s"' % arg)
+		self.cur.execute('DELETE FROM GOODS WHERE NAME = "%s" LIMIT 1' % arg)
 		self.conn.commit()
 	def do_list(self, arg):
 		'列出所有物品。'
 		self.cur.execute('SELECT * FROM GOODS')
-		for record in self.cur.fetchall(): print(record)
+		self.print_records()
 	def do_search(self, arg):
 		'查找物品。'
 		self.cur.execute('SELECT * FROM GOODS WHERE NAME LIKE "%%%s%%"' % arg)
-		for record in self.cur.fetchall(): print(record)
+		self.print_records()
 	def do_reset(self, arg):
 		'重置数据库。'
 		self.cur.execute('DROP TABLE GOODS')
-		self.cur.execute('CREATE TABLE GOODS(ID INT PRIMARY KEY NOT NULL, NAME TEXT NOT NULL);')
+		self.cur.execute('CREATE TABLE GOODS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT NOT NULL);')
 		self.conn.commit()
 	def do_exit(self, arg):
 		'退出交互式环境。'
